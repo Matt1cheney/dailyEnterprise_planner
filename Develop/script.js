91c7e24aa8eca2f9d6b5a39921a80458351af5d7
Matthew Cheney <matt1cheney@gmail.com>
2020-02-01 17:36:52 -0500

    got the classes to apply to the correct textarea

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 $(function() {
+  //create twos constants for the date display
   const todaysDate = $("#currentDay");
   const whatTimeIsIt = moment().format("MMMM Do YYYY");
   console.log(moment());
@@ -21,32 +22,31 @@ $(function() {
     moment().hour(19),
     moment().hour(20)
   ];
-  
-  // i need to tell the computer what time the timeblocks represent by calling a function that differentiates the times.
-  if (timeVal[15] < moment()) {
-    console.log("itworkingyo");
-  }
- // moment().hour(10).diff(moment("hours"))
-  // moment.duration().hours();
-  // moment.duration().asHours();
+
+ 
 
   function hourChange() {
     let description = $("textarea");
-    // let thePastHour = false;
-    //  let thePresentHour = false;
-    // let theFutureHour = false;
+    let descriptionNumber = 0;
+    let forHourCounter = 5;
+    
+    console.log(moment().startOf("hour"));
     timeVal.forEach(hour => {
       
-      
-      if (timeVal < moment(0).diff(moment("hours"))
-        description.addClass("past");
-      } else if (timeVal > moment().startOf(timeVal).fromNow) {
-        description.addClass("future");
-        // theFutureHour == true;
-        console.log("nowhere");
-      } else if (timeVal === moment()) {
-        description.addClass("present");
+
+      let someMomentHour = moment().hour(forHourCounter);
+      let selector = `.hour-${someMomentHour.format("HH")} `;
+
+      let myElem = $(selector);
+
+      if (0 > hour.diff(moment(), "hours")) {
+        myElem.addClass("past");
+      } else if (0 < hour.diff(moment().startOf("hour"), "hours")) {
+        myElem.addClass("future");
+      } else {
+        myElem.addClass("present");
       }
+      forHourCounter++;
     });
   }
 
