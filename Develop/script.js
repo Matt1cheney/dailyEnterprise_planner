1d0549a5733f736524df031f32e99e168a0f12fe
Matthew Cheney <matt1cheney@gmail.com>
2020-02-02 10:53:27 -0500

    add favicon,  save local storage

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,9 @@ $(function() {
   //create twos constants for the date display
   const todaysDate = $("#currentDay");
   const whatTimeIsIt = moment().format("MMMM Do YYYY");
-  console.log(moment());
+  //applying the date to the HTML page
   todaysDate.text(whatTimeIsIt);
+  //creating an array of "moment" objects
   let timeVal = [
     moment().hour(05),
     moment().hour(06),
@@ -23,32 +24,55 @@ $(function() {
     moment().hour(20)
   ];
 
- 
-
+  // creating a function to apply classes to the appropriate hour blocks
   function hourChange() {
-    let description = $("textarea");
-    let descriptionNumber = 0;
+    // Retrieve
+    //$(document).getItem(`.hour-${thisTextBlock.format("HH")} `);
+    //.innerHTML = localStorage.getItem("lastname");
+    //create variable for looping through hour blocks that start at 5 AM
     let forHourCounter = 5;
-    
-    console.log(moment().startOf("hour"));
+
+    //start forEach loop to evaluate what hour it is and apply the past, present, or future classes
     timeVal.forEach(hour => {
-      
+      // someMomentHour is grabbing the current hour and passing in forHourCounter created above
+      let thisTextBlock = moment().hour(forHourCounter);
+      // forming my hour-x class to be able to reference each time block
+      let referredBlock = `.hour-${thisTextBlock.format("HH")} `;
 
-      let someMomentHour = moment().hour(forHourCounter);
-      let selector = `.hour-${someMomentHour.format("HH")} `;
-
-      let myElem = $(selector);
-
+      let myElement = $(referredBlock);
+      // if 0 is greater than any hour past the current apply this class
       if (0 > hour.diff(moment(), "hours")) {
-        myElem.addClass("past");
+        myElement.addClass("past");
+        //if 0 is less than any hourblock from the current moment, apply this class
       } else if (0 < hour.diff(moment().startOf("hour"), "hours")) {
-        myElem.addClass("future");
+        myElement.addClass("future");
+        // anything 0 is current, apply this class
       } else {
-        myElem.addClass("present");
+        myElement.addClass("present");
       }
       forHourCounter++;
     });
+    let thisTextBlock = moment().hour(forHourCounter-1);
+      // forming my hour-x class to be able to reference each time block
+      let referredBlock = $(`.hour-${thisTextBlock.format("HH")}`).val() ;
+        $(document).on("click",".saveBtn", function() {
+          $(".fa-save")
+            .addClass("animated")
+            .addClass("wobble");
+            // $(".fa-save").removeClass("animated").removeClass("wobble");
+          // Store
+          //get the text
+          // get the text
+          // var textSave = $(".hour-05").text(textSave)
+    
+          localStorage.setItem('planSave', $(JSON.stringify(referredBlock.innerText)));
+          console.log(`.hour-${thisTextBlock.format("HH")} `)
+          
+            // test if it works
+          });
+    //});
   }
 
   hourChange();
+
 });
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dailyEnterprise_planner
+
+GitHub Repo: https://github.com/Matt1cheney/dailyEnterprise_planner
+URL: https://matt1cheney.github.io/dailyEnterprise_planner/
+
